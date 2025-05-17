c800e1a140a399ef334d95c2e4aad51e61fb53fc
PedroH_Jesus <pedrohdev01@gmail.com>
2025-05-17 10:45:23 -0300

    remover class

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Home from "./pages/Home"
 function App() {
   return (
     <Router>
-      <main className="min-h-screen flex flex-col bg-light font-sans text-dark">
+      <main className="min-h-screen flex flex-col bg-light">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
