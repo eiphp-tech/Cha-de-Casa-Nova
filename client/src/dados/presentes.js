7d8e13a14b6f3adbfd558d956e7eb0ec9e30b61f
PedroH_Jesus <pedrohdev01@gmail.com>
2025-06-05 21:25:22 -0300

    salvar tudo

---
+++ client/src/dados/presentes.js
@@ -0,0 +1,16 @@
+const presentes = [
+  {
+    id: 1,
+    title: "",
+    imageURL: "",
+    link: "",
+  },
+  {
+    id: 2,
+    title: "",
+    imageURL: "",
+    link: "",
+  },
+]
+
+export default presentes
