3c98076c7c1d9c3200b234a78c126dce85194462
PedroH_Jesus <pedrohdev01@gmail.com>
2025-06-07 18:36:12 -0300

    add image no catalogo

--- client/src/assets/image/presentes/imageCatalogo.jsx
+++ client/src/assets/image/presentes/imageCatalogo.jsx
@@ -1,9 +1,73 @@
 import Facas from "./jogo-de-facas.png"
-import sanduicheira from "./sanduicheira.png"
+import Sanduicheira from "./sanduicheira.png"
+import assadeira from "./assadeira-com-tampa.png"
+import cestoLixo from "./cesto-de-lixo.png"
+import coberdrom from "./coberdrom.png"
+import jogoDeCopos from "./jogo-de-copos.png"
+import jogoDeLencol from "./jogo-de-lencol.png"
+import jogoDePotes from "./jogo-de-potes-de-plastico.png"
+import jogoDeCaneca from "./jogo-de-caneca.png"
+import jogoAmericano from "./jogo-americano.png"
+import jogoDeFormas from "./jogo-de-formas.png"
+import jogoDePratos from "./jogo-de-pratos.png"
+import jogoDeTalheres from "./jogo-de-talheres.png"
+import jogoDeToalhas from "./jogo-de-toalhas.png"
+import KitDeBowls from "./kit-de-bowls.png"
+import escorredores from "./kit-de-escorredores.png"
+import ferramentas from "./kit-de-ferramentas.png"
+import lavabo from "./kit-de-lavabo.png"
+import medidores from "./kit-de-medidores.png"
+import manta from "./manta-cobertor.png"
+import organizaodorLanvaderia from "./organizador-lavanderia.png"
+import organizaodorGeladeira from "./organizadores-de-geladeira.png"
+import panelaP from "./panela-de-pressao.png"
+import peneira from "./peneira-de-inox.png"
+import detergente from "./porta-detergente.png"
+import frios from "./porta-frios.png"
+import temperos from "./porta-temperos.png"
+import ralador from "./ralador.png"
+import tabuaDeCorte from "./tabua-de-corte.png"
+import ventilador from "./ventilador.png"
+import chaleira from "./chaleira-eletrica.png"
+import cumbucas from "./cumbucas.png"
+import talher from "./escorredor-de-talher.png"
+import galheteiro from "./galheteiro-de-vidro.png"
 
 const imageCatalog = {
   "jogo-de-facas": Facas,
-  sanduicheira: sanduicheira,
+  sanduicheira: Sanduicheira,
+  assadeira: assadeira,
+  "cesto-de-lixo": cestoLixo,
+  coberdrom: coberdrom,
+  "jogo-de-copos": jogoDeCopos,
+  "jogo-de-lencol": jogoDeLencol,
+  "jogo-de-potes": jogoDePotes,
+  "jogo-de-caneca": jogoDeCaneca,
+  "jogo-americano": jogoAmericano,
+  "jogo-de-formas": jogoDeFormas,
+  "jogo-de-pratos": jogoDePratos,
+  "jogo-de-talheres": jogoDeTalheres,
+  "jogo-de-toalhas": jogoDeToalhas,
+  "kit-de-bowls": KitDeBowls,
+  "kit-de-escorredores": escorredores,
+  "kit-de-ferramentas": ferramentas,
+  "kit-de-lavabo": lavabo,
+  "kit-de-medidores": medidores,
+  manta: manta,
+  "organizador-lavanderia": organizaodorLanvaderia,
+  "organizadores-de-geladeira": organizaodorGeladeira,
+  "panela-de-pressao": panelaP,
+  "peneira-de-inox": peneira,
+  "porta-detergente": detergente,
+  "porta-frios": frios,
+  "porta-temperos": temperos,
+  ralador: ralador,
+  "tabua-de-corte": tabuaDeCorte,
+  ventilador: ventilador,
+  chaleira: chaleira,
+  cumbucas: cumbucas,
+  "escorredor-de-talher": talher,
+  "galheteiro-de-vidro": galheteiro,
 }
 
 export default imageCatalog
