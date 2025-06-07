24fb9b8c226e1688f08fee9605631dd4f9d4418d
PedroH_Jesus <pedrohdev01@gmail.com>
2025-06-06 22:13:38 -0300

    salvar alterações

--- client/src/components/cards.jsx
+++ client/src/components/cards.jsx
@@ -1,4 +1,14 @@
-const Cards = ({ imageURL, title, link, reservado, onReservar }) => {
+import { BookmarkPlus, BookmarkCheck } from "lucide-react"
+import React, { useState } from "react"
+
+const Cards = ({ imageURL, title, link }) => {
+  const [reserved, setReserved] = useState(false)
+
+  const handleReserve = () => {
+    if (!reserved) {
+      setReserved(true)
+    }
+  }
   return (
     <section className="w-80 bg-white rounded-xl shadow-md overflow-hidden">
       <div className="h-40 bg-gray-300 flex items-center justify-center">
@@ -9,31 +19,37 @@ const Cards = ({ imageURL, title, link, reservado, onReservar }) => {
         />
       </div>
       <div className="p-4 flex flex-col gap-2">
-        <h3 className="text-lg font-instru">{title}</h3>
+        <h3 className="text-lg font-instru font-semibold">{title}</h3>
         <div className="flex justify-between items-center">
           <button
-            onClick={onReservar}
+            Add
+            commentMore
+            actions
+            onClick={handleReserve}
             className={`flex items-center gap-1 text-sm ${
-              reservado ? "text-green-600" : "text-gray-700"
+              reserved ? "text-green-600" : "text-gray-700"
             }`}
           >
-            {reservado ? (
+            {reserved ? (
               <BookmarkCheck size={18} />
             ) : (
               <BookmarkPlus size={18} />
             )}
-            {reservado ? "Reservado" : "Reservar"}
+            {reserved ? "Reservado" : "Reservar"}
           </button>
+
           <a
             href={link}
             target="_blank"
             rel="noopener noreferrer"
             className="bg-blue-500 hover:bg-blue-600 text-white text-sm px-3 py-1 rounded"
           >
-            Link
+            Ver Sugestão
           </a>
         </div>
       </div>
     </section>
   )
 }
+
+export default Cards
