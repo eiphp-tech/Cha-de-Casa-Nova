6d8b9bd9a4b2a599389f9d9f394c9a3c35174a29
PedroH_Jesus <pedrohdev01@gmail.com>
2025-08-18 21:28:38 -0300

    atualizar o imageCatalogo

--- client/src/assets/image/imageCatalogo.jsx
+++ client/src/assets/image/imageCatalogo.jsx
@@ -1,71 +1,37 @@
-import batedeira from "./presentes/batedeira.png"
-import Sanduicheira from "./presentes/Sanduicheira.png"
-import airFryer from "./presentes/airFryer.png"
-import AparelhoDeJantar from "./presentes/AparelhoDeJantar.png"
-import Assadeiras from "./presentes/Assadeiras.png"
-import assadeirasMarinex from "./presentes/assadeirasMarinex.png"
-import Bowl from "./presentes/Bowl.png"
-import Coberta from "./presentes/Coberta.png"
-import Condimentos from "./presentes/Condimentos.png"
-import ConjuntoEscorredor from "./presentes/ConjuntoEscorredor.png"
-import Cortina from "./presentes/Cortina.png"
-import Escorredor from "./presentes/Escorredor.png"
-import FacasDeCarne from "./presentes/FacasDeCarne.png"
-import JogoAmericano from "./presentes/JogoAmericano.png"
-import JogoDeCopos from "./presentes/JogoDeCopos.png"
-import JogoDeToalhas from "./presentes/JogoDeToalhas.png"
-import JogoTapete from "./presentes/JogoTapete.png"
-import KitPanos from "./presentes/KitPanosDePrato.png"
-import KitPeneira from "./presentes/KitPeneira.png"
-import KitPia from "./presentes/KitPiaDeCozinha.png"
-import liquidificador from "./presentes/liquidificador.png"
-import Lixeira from "./presentes/Lixeira.png"
-import medidores from "./presentes/medidores.png"
-import PanelaDePressao from "./presentes/PanelaDePressao.png"
-import pecaInox from "./presentes/pecaInox.png"
-import PortaFrios from "./presentes/PortaFrios.png"
-import Hermeticos from "./presentes/PotesHermeticos.png"
-import PotesVidro from "./presentes/PotesVidro.png"
-import Tabua from "./presentes/TabuaDeCorte.png"
-import Taca from "./presentes/TacaDiamond.png"
-import Talheres from "./presentes/Talheres.png"
-import Tigela from "./presentes/TigelaBowl.png"
-import Utensilios from "./presentes/Utensilios.png"
+/**
+ * Esta função importa dinamicamente todas as imagens da pasta "./presentes".
+ * Ela cria um catálogo de imagens onde a chave é o nome do arquivo (sem a extensão)
+ * e o valor é o módulo da imagem importada.
+ *
+ * Utiliza `import.meta.glob` do Vite para encontrar todos os arquivos que correspondem
+ * ao padrão, importando-os de forma "eager" (imediata).
+ */
+function createImageCatalog() {
+  // O padrão './presentes/*.png' busca por todos os arquivos .png na pasta adjacente 'presentes'.
+  // { eager: true } garante que os módulos sejam importados imediatamente.
+  const imageModules = import.meta.glob("./presentes/*.png", { eager: true })
 
-const imageCatalog = {
-  batedeira: batedeira,
-  Sanduicheira: Sanduicheira,
-  airFryer: airFryer,
-  AparelhoDeJantar: AparelhoDeJantar,
-  Assadeiras: Assadeiras,
-  assadeirasMarinex: assadeirasMarinex,
-  Bowl: Bowl,
-  Coberta: Coberta,
-  Condimentos: Condimentos,
-  ConjuntoEscorredor: ConjuntoEscorredor,
-  Cortina: Cortina,
-  Escorredor: Escorredor,
-  FacasDeCarne: FacasDeCarne,
-  JogoAmericano: JogoAmericano,
-  JogoDeCopos: JogoDeCopos,
-  JogoDeToalhas: JogoDeToalhas,
-  JogoTapete: JogoTapete,
-  KitPanosDePrato: KitPanos,
-  KitPeneira: KitPeneira,
-  KitPiaDeCozinha: KitPia,
-  liquidificador: liquidificador,
-  Lixeira: Lixeira,
-  medidores: medidores,
-  PanelaDePressao: PanelaDePressao,
-  pecaInox: pecaInox,
-  PortaFrios: PortaFrios,
-  PotesHermeticos: Hermeticos,
-  PotesVidro: PotesVidro,
-  TabuaDeCorte: Tabua,
-  TacaDiamond: Taca,
-  Talheres: Talheres,
-  TigelaBowl: Tigela,
-  Utensilios: Utensilios,
+  const imageCatalog = Object.entries(imageModules).reduce(
+    (acc, [path, module]) => {
+      // Extrai o nome do arquivo do caminho (ex: './presentes/batedeira.png' -> 'batedeira.png')
+      const fileName = path.split("/").pop()
+
+      // Remove a extensão do arquivo para criar uma chave limpa (ex: 'batedeira.png' -> 'batedeira')
+      // A expressão regular /\.\w+$/ remove o último ponto e a extensão que o segue.
+      const key = fileName.replace(/\.\w+$/, "")
+
+      // Atribui o módulo da imagem (o caminho da imagem) à chave correspondente no nosso catálogo.
+      // A propriedade 'default' geralmente contém o caminho da imagem processada pelo bundler.
+      acc[key] = module.default
+
+      return acc
+    },
+    {}
+  )
+
+  return imageCatalog
 }
+
+const imageCatalog = createImageCatalog()
 
 export default imageCatalog
