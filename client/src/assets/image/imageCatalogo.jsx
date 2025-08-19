e340e222652eb642f2722edc80f00e6c54329ec3
PedroH_Jesus <pedrohdev01@gmail.com>
2025-08-18 21:19:45 -0300

    bug corrigido

--- client/src/assets/image/imageCatalogo.jsx
+++ client/src/assets/image/imageCatalogo.jsx
@@ -1,49 +1,71 @@
 import batedeira from "./presentes/batedeira.png"
-import talheres from "./presentes/Talheres.png"
-import JogoDeCopos from "./presentes/JogoDeCopos.png"
-import liquidificador from "./presentes/liquidificador.png"
+import Sanduicheira from "./presentes/Sanduicheira.png"
+import airFryer from "./presentes/airFryer.png"
+import AparelhoDeJantar from "./presentes/AparelhoDeJantar.png"
+import Assadeiras from "./presentes/Assadeiras.png"
+import assadeirasMarinex from "./presentes/assadeirasMarinex.png"
+import Bowl from "./presentes/Bowl.png"
+import Coberta from "./presentes/Coberta.png"
+import Condimentos from "./presentes/Condimentos.png"
+import ConjuntoEscorredor from "./presentes/ConjuntoEscorredor.png"
+import Cortina from "./presentes/Cortina.png"
+import Escorredor from "./presentes/Escorredor.png"
+import FacasDeCarne from "./presentes/FacasDeCarne.png"
 import JogoAmericano from "./presentes/JogoAmericano.png"
-import Toalhas from "./presentes/JogoDeToalhas.png"
-import Tapete from "./presentes/JogoTapete.png"
-import PanoDePrato from "./presentes/KitPanosDePrato.png"
-import Peneira from "./presentes/KitPeneira.png"
-import PiaDeCozinha from "./presentes/KitPiaDeCozinha.png"
+import JogoDeCopos from "./presentes/JogoDeCopos.png"
+import JogoDeToalhas from "./presentes/JogoDeToalhas.png"
+import JogoTapete from "./presentes/JogoTapete.png"
+import KitPanos from "./presentes/KitPanosDePrato.png"
+import KitPeneira from "./presentes/KitPeneira.png"
+import KitPia from "./presentes/KitPiaDeCozinha.png"
+import liquidificador from "./presentes/liquidificador.png"
 import Lixeira from "./presentes/Lixeira.png"
-import Medidores from "./presentes/medidores.png"
-import PanelaPressao from "./presentes/PanelaDePressao.png"
+import medidores from "./presentes/medidores.png"
+import PanelaDePressao from "./presentes/PanelaDePressao.png"
 import pecaInox from "./presentes/pecaInox.png"
 import PortaFrios from "./presentes/PortaFrios.png"
 import Hermeticos from "./presentes/PotesHermeticos.png"
 import PotesVidro from "./presentes/PotesVidro.png"
 import Tabua from "./presentes/TabuaDeCorte.png"
-import TacaVidro from "./presentes/TacaDiamond.png"
+import Taca from "./presentes/TacaDiamond.png"
+import Talheres from "./presentes/Talheres.png"
 import Tigela from "./presentes/TigelaBowl.png"
 import Utensilios from "./presentes/Utensilios.png"
-import Sanduicheira from "./presentes/Sanduicheira.png"
 
 const imageCatalog = {
   batedeira: batedeira,
-  JogoDeCopos: JogoDeCopos,
-  liquidificador: liquidificador,
+  Sanduicheira: Sanduicheira,
+  airFryer: airFryer,
+  AparelhoDeJantar: AparelhoDeJantar,
+  Assadeiras: Assadeiras,
+  assadeirasMarinex: assadeirasMarinex,
+  Bowl: Bowl,
+  Coberta: Coberta,
+  Condimentos: Condimentos,
+  ConjuntoEscorredor: ConjuntoEscorredor,
+  Cortina: Cortina,
+  Escorredor: Escorredor,
+  FacasDeCarne: FacasDeCarne,
   JogoAmericano: JogoAmericano,
-  talheres: talheres,
-  Toalhas: Toalhas,
-  Tapete: Tapete,
-  PanoDePrato: PanoDePrato,
-  Peneira: Peneira,
-  PiaDeCozinha: PiaDeCozinha,
+  JogoDeCopos: JogoDeCopos,
+  JogoDeToalhas: JogoDeToalhas,
+  JogoTapete: JogoTapete,
+  KitPanosDePrato: KitPanos,
+  KitPeneira: KitPeneira,
+  KitPiaDeCozinha: KitPia,
+  liquidificador: liquidificador,
   Lixeira: Lixeira,
-  Medidores: Medidores,
-  PanelaPressao: PanelaPressao,
+  medidores: medidores,
+  PanelaDePressao: PanelaDePressao,
   pecaInox: pecaInox,
   PortaFrios: PortaFrios,
-  Hermeticos: Hermeticos,
+  PotesHermeticos: Hermeticos,
   PotesVidro: PotesVidro,
-  Tabua: Tabua,
-  TacaVidro: TacaVidro,
-  Tigela: Tigela,
+  TabuaDeCorte: Tabua,
+  TacaDiamond: Taca,
+  Talheres: Talheres,
+  TigelaBowl: Tigela,
   Utensilios: Utensilios,
-  Sanduicheira: Sanduicheira,
 }
 
 export default imageCatalog
